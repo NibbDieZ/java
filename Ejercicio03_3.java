5b221df6cf90f08613be2278498e43e78a75e410
Nicolás Ignacio Barboza B <n.barboza01@ufromail.cl>
2016-05-13 08:05:12 -0400

    Update and rename Ejercicio03.java to Ejercicio03_3.java

--- Ejercicio03_3.java
+++ Ejercicio03_3.java
@@ -10,7 +10,7 @@
  */
 import java.util.Scanner;
 
-public class Ejercicio03 {
+public class Ejercicio03_3 {
     public static void main (String[] args){
         Scanner intro = new Scanner (System.in);
         System.out.println ("Ingrese edad");
