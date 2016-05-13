647dc20e9675ca5c45616cc43b27085fe5675fb7
Nicolás Ignacio Barboza B <n.barboza01@ufromail.cl>
2016-05-13 08:01:28 -0400

    Update and rename Ejercicio01.java to Ejercicio01_3.java

--- Ejercicio01_3.java
+++ Ejercicio01_3.java
@@ -10,7 +10,7 @@
  */
 import java.util.Scanner;
 
-public class Ejercicio01 {
+public class Ejercicio01_3 {
     public static void main (String[] args){
         Scanner intro = new Scanner (System.in);
         System.out.println ("Ingrese radio del cilindro");
